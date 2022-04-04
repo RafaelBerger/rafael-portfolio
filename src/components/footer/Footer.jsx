a7457bbf097e312f2a2ef040c50c9b920f46f355
Rafael <rafael.berger20@gmail.com>
2022-04-04 18:38:31 -0300

    footer concluído

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <section id="portfolio">Footer</section>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Rafael Berger
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Skills</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://linkedin.com/in/rafael-berger">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/RafaelBerger">
+          <FaGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Rafael de Andrade Berger. All rights reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
